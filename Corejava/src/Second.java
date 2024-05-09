21c6026cc75854d877a194c3f3d101ddbfec5c39
MAHESH BABU <MAHESH BABU@DESKTOP-E82UG7J>
2024-05-09 18:24:22 +0530

    next

---
+++ Corejava/src/Second.java
@@ -0,0 +1,10 @@
+public class Second {
+
+	public static void main(String[] args) {
+		System.out.println("HI... Naresh");
+		for( int i=0; i<5; i++) {
+		 System.out.println(i);
+		}
+	}
+
+}
---
+++ Corejava/src/Secondpro.java
@@ -0,0 +1,11 @@
+
+public class Secondpro {
+
+	public static void main(String[] args) {
+		System.out.println("HI... Naresh");
+		for( int i=0; i<5; i++) {
+		 System.out.println(i);
+		}
+	}
+
+}
